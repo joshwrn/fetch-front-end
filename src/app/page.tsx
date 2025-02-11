39d5f439d4d4c231b034fd0e41afd4bf33ca4c7d
josh warren <joshnwarren@gmail.com>
2025-02-10 18:04:10 -0800

    feat: improve sign in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,10 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { useRouter } from 'next/navigation'
 import { dogUrl } from '@/lib/dog-url'
+import Image from 'next/image'
+
+import dog_image from '@/public/dog.webp'
+import { ProgressiveImage } from '@/components/ui/progressive-image'
 
 const formSchema = z.object({
   name: z.string().min(3),
@@ -54,43 +58,57 @@ export default function Home() {
 
   return (
     <div className="bg-orange-100 h-screen flex flex-col justify-center items-center">
-      <FormProvider {...form}>
-        <form
-          className="flex flex-col gap-4 p-8 rounded-lg bg-orange-50 shadow-md max-w-full w-[400px]"
-          onSubmit={(e) => {
-            e.preventDefault()
-            form.handleSubmit((input) => loginMutation.mutate(input))()
-          }}
-        >
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="name" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+      <div className="w-full max-w-[800px] h-fit md:h-full max-h-[500px] flex rounded-lg bg-orange-50 shadow-md overflow-hidden relative">
+        <div className="w-full max-w-[600px] h-full hidden md:flex overflow-hidden relative">
+          <ProgressiveImage
+            src={dog_image}
+            width={500}
+            height={500}
+            alt="dog"
+            className="object-cover object-bottom w-full h-full"
           />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </FormProvider>
+        </div>
+        <FormProvider {...form}>
+          <form
+            className="flex flex-col gap-4 p-8 w-full md:max-w-[350px] overflow-hidden"
+            onSubmit={(e) => {
+              e.preventDefault()
+              form.handleSubmit((input) => loginMutation.mutate(input))()
+            }}
+          >
+            <h1 className="text-xl font-bold text-orange-950">Login</h1>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="bg-orange-950 hover:bg-orange-900">
+              Submit
+            </Button>
+          </form>
+        </FormProvider>
+      </div>
     </div>
   )
 }
